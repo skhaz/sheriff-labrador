c8998bbec3692c261571e799250faf0162ddb060
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-08-19 09:41:54 -0300

    Add Redis

--- handler.py
+++ handler.py
@@ -1,4 +1,15 @@
+import os
+
+from redis import ConnectionPool
+from redis import Redis
+
+redis_pool = ConnectionPool.from_url(os.environ["REDIS_DSN"])
+redis = Redis(connection_pool=redis_pool)
+
+
 def telegram(event, context):
+    redis.ping()
+
     return {
         "statusCode": 200,
     }
