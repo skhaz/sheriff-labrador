ee6f3a2b5bc6dcb79ff50e7b9fadf6d96592bc4e
Rodrigo Delduca <46259+skhaz@users.noreply.github.com>
2023-08-22 11:28:01 -0300

    Work in progress

--- handler.py
+++ handler.py
@@ -101,7 +101,7 @@ async def temp(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     if not message:
         return
 
-    await message.reply_text("Ok")
+    await message.reply_photo("https://vy20xyg6p0.execute-api.us-east-1.amazonaws.com/?text=ABCD")
 
 
 application.add_handler(CommandHandler("temp", temp))
